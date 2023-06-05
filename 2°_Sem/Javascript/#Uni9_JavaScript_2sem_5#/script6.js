9ef824ff652d9b5d642367fc158c7970c3347acf
Yuri <80179611+JapaScripter@users.noreply.github.com>
2023-06-05 10:22:37 -0300

    Add files via upload

---
+++ 2°_Sem/Javascript/#Uni9_JavaScript_2sem_5#/script6.js
@@ -0,0 +1,25 @@
+/* =====Colocar Linha===== */
+document.write("<hr>");
+/* =====Colocar Linha===== */
+
+/* =====Titulo===== */
+document.write("Teste 6 - Verifica Estado");
+/* =====Titulo===== */
+
+/* =====Pular Linha===== */
+document.write("<br>");
+document.write("<br>");
+/* =====Pular Linha===== */
+
+/* =====Declarar Variáveis===== */
+var i;
+i = 5;
+/* =====Declarar Variáveis===== */
+
+/* =====Printar Variáveis===== */
+do {
+	document.write("O valor de i é = " + i + "<br>");
+	i++;
+} while (i < 10);
+document.write("Se o valor for < que 10 ele vai contar até aqui ser: " + i);
+/* =====Printar Variáveis===== */
