43c6147773e4233d129392165e3b7bc318b1da83
Yuri <80179611+JapaScripter@users.noreply.github.com>
2023-06-05 10:21:58 -0300

    Add files via upload

---
+++ 2°_Sem/#Uni9_JavaScript_2sem_5#/script7.js
@@ -0,0 +1,24 @@
+/* =====Colocar Linha===== */
+document.write("<hr>");
+/* =====Colocar Linha===== */
+
+/* =====Titulo===== */
+document.write("Teste 7 - Printar 0, 1, 2 em 3 vezes");
+/* =====Titulo===== */
+
+/* =====Pular Linha===== */
+document.write("<br>");
+document.write("<br>");
+/* =====Pular Linha===== */
+
+/* =====Declarar Variáveis===== */
+var i, j;
+/* =====Declarar Variáveis===== */
+
+/* =====Printar Variáveis===== */
+for (i = 0; i < 3; i++) {
+	for (j = 1; j < 4; j++) {
+		document.write(i + " - " + j + "°<br>");
+	}
+}
+/* =====Printar Variáveis===== */
